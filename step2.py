f7c804051a4c248902da9866aa4e52d9e5e5a3ed
Benji <97198967+BenPI88@users.noreply.github.com>
2023-06-04 18:46:41 -0500

    Create step2.py

---
+++ step2.py
@@ -0,0 +1,4 @@
+os.system("sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE")
+os.system("sudo iptables -A FORWARD -i wlan0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT")
+os.system("sudo iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT")
+print("If there is an error above, restart the pi. Otherwise move on to step 3!")
