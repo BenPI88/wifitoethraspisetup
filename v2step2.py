ca6ba0aadf09839a105ea2001242dee1f52f23ad
Benji <97198967+BenPI88@users.noreply.github.com>
2023-06-04 20:26:21 -0500

    Update v2step2.py

--- v2step2.py
+++ v2step2.py
@@ -9,4 +9,20 @@ os.system("sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE")
 os.system("sudo iptables -A FORWARD -i wlan0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT")
 os.system("sudo iptables -A FORWARD =i eth0 -o wlan0 -j ACCEPT")
 os.system('sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"')
-rc = open("/etc/rc.local")
+def removelastline(file):
+  with open(file, 'r+') as fp:
+    # read an store all lines into list
+    lines = fp.readlines()
+    # move file pointer to the beginning of a file
+    fp.seek(0)
+    # truncate the file
+    fp.truncate()
+
+    # start writing lines except the first line
+    # lines[1:] from line 2 to last line
+    fp.writelines(lines[1:])
+    fp.close()
+removelastline("/etc/rc.local")
+rc = open("/etc/rc.local", "a")
+rc.write("\niptables-restore < /etc/iptables.ipv4.nat\nexit 0")
+rc.close
